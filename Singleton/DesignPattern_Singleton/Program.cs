77db1e4eb009823367479a81534156f1e09cdcfe
yuhsiang237 <umworksite@gmail.com>
2022-01-27 00:33:53 +0800

    add design pattern

---
+++ Singleton/DesignPattern_Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPattern_Singleton
+{
+    /// <summary>
+    /// Singleton模式
+    /// 只有一個實例，程式被啟動時建立出來，直到關閉才被刪除。
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+            if(ReferenceEquals(singleton1, singleton2))
+            {
+                Console.WriteLine("SameReference");
+            }
+        }
+    }
+}
---
+++ Singleton/DesignPattern_Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _theInstance = null;
+
+        private Singleton()
+        {
+
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                if(_theInstance == null) // 僅實例一次
+                {
+                    _theInstance = new Singleton();
+                }
+                return _theInstance;
+            }
+        }
+    }
+}
